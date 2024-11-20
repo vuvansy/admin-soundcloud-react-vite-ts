e045a093de8a39275dcfc2ac9ae5a8f46e32d481
vuvansy <vuvansy3001@gmail.com>
2024-11-20 23:03:34 +0700

    Cài thư viện React Router, 46. Tạo Route Users , cài thư viện Atd

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/users",
+        element: <div>Manage users</div>,
+    },
+    {
+        path: "/tracks",
+        element: <div>Manage tracks</div>,
+    },
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
