40c348ad797206c3b73b018bbd884910736e4521
vuvansy <vuvansy3001@gmail.com>
2024-11-21 13:56:29 +0700

    Design giao diện Admin

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import UsersPage from "./screens/users.page.tsx";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+    createBrowserRouter,
+    Link,
+    Outlet,
+    RouterProvider,
+} from "react-router-dom";
+import { TeamOutlined, FireOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+
+const items: MenuProps["items"] = [
+    {
+        label: <Link to={"/"}>Home</Link>,
+        key: "home",
+        icon: <FireOutlined />,
+    },
+    {
+        label: <Link to="/users">Manage Users</Link>,
+        key: "users",
+        icon: <TeamOutlined />,
+    },
+];
+
+const Header = () => {
+    const [current, setCurrent] = useState("home");
+    const onClick: MenuProps["onClick"] = (e) => {
+        setCurrent(e.key);
+    };
+
+    return (
+        <Menu
+            onClick={onClick}
+            selectedKeys={[current]}
+            mode="horizontal"
+            items={items}
+        />
+    );
+};
+
+const LayoutAdmin = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+        </div>
+    );
+};
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
+        element: <LayoutAdmin />,
+        children: [
+            { index: true, element: <App /> },
+            {
+                path: "users",
+                element: <UsersPage />,
+            },
+        ],
     },
-    {
-        path: "/users",
-        element: <div>Manage users</div>,
-    },
+
     {
         path: "/tracks",
         element: <div>Manage tracks</div>,
     },
 ]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <RouterProvider router={router} />
---
+++ src/screens/users.page.tsx
@@ -0,0 +1,9 @@
+import UsersTable from "../components/users/users.table";
+const UsersPage = () => {
+    return (
+        <div>
+            <UsersTable />
+        </div>
+    );
+};
+export default UsersPage;
